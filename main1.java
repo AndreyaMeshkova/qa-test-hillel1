1313f55f51bfe96ad35d20253c37f170b0c47996
AndreyaMeshkova <a.meshkova95@gmail.com>
2023-11-05 23:37:44 +0200

    check new fuction

--- main1.java
+++ main1.java
@@ -1,6 +1,7 @@
 public static void main(String[] args) {
         t = 1;
         y = 2;
-        System.out.println(t);
-         System.out.println(y);
+        f = t +y;
+        System.out.println(f);
+        
     }
